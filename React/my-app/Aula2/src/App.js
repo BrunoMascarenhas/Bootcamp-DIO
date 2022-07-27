79af3d6c01114cbd0164154a9e0df824ad78c853
Bruno <brunokarkara@gmail.com>
2022-07-27 14:17:43 -0300

    ADD projeto Linux

--- React/my-app/Aula2/src/App.js
+++ React/my-app/Aula2/src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 
 const buttonA = <button> Histórico de Clientes</button>
 
-const buttonB = <button> Segundo Botão </button>
+const buttonB = <button> Cadastrar Cliente </button>
 
 const hasCustomer = true
 
 const App = () => {
-    return (
+     const renderShowHistory = (
         <div>
             <p>Digital Innovation</p>
             <p> Bem Vindo =D.</p>
@@ -20,6 +20,37 @@ const App = () => {
             </div>
             )}        
         </div>
-    );
+    )
+    const renderAddCustomer = (
+        <div>
+            Clique abaixo para cadastrar o cliente
+            <br />
+            {buttonB}
+        </div>
+    )
+    const showCustomer = () =>{
+        if (!hasCustomer) return null
+
+        return(
+            <div>
+                <h2>Cliente Bruno Mascarenhas da Silva</h2>
+            </div>
+        )
+    }
+    console.log('hasCustomer', hasCustomer)
+
+    return(
+        <div>
+            <p> Digital Innovation One</p>
+            <p>Bem Vindo a nossa aual =D</p>
+            {hasCustomer ? renderShowHistory : rednerAddCustomer}
+
+            <div>
+                {showCustomer()}
+                </div>
+        </div>        
+
+    )
+
 };
 export default App;
